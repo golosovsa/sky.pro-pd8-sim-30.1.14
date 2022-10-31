3a48261cbf63403cab36626334ef503e4168a9e6
grm <golosov_SA@mail.ru>
2022-10-31 10:53:19 +0300

    custom_user_1 done

--- part1/custom_user_1/custom_user_1/models.py
+++ part1/custom_user_1/custom_user_1/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,5 +6,6 @@ from django.db import models
 # TODO Профиль пользователя должен иметь следующие поля
 # адрес (address) длинной не более 50 символов
 # телефон (phone) - номер должен храниться в формате
-class Profile:
-    pass
+class Profile(User):
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
--- part1/custom_user_1/my_project/settings.py
+++ part1/custom_user_1/my_project/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH_USER_MODEL = 'custom_user_1.Profile'
