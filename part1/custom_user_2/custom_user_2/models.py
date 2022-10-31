af01c5c8c34464c674f1d7cfa916f8394509f299
grm <golosov_SA@mail.ru>
2022-10-31 11:01:43 +0300

    custom_user_2 done

--- part1/custom_user_2/custom_user_2/models.py
+++ part1/custom_user_2/custom_user_2/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # TODO опишите модель кастомного пользователя ниже
 class CustomUser(AbstractUser):
-    pass
+    TEACHER = "teacher"
+    STUDENT = "student"
+    ROLE = [("teacher", TEACHER), ("student", STUDENT)]
+
+    role = models.CharField(max_length=7, choices=ROLE)
+    birth_date = models.DateField()
